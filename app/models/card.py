34140a887bf6ebe9b3d36b07ce1936e70d58d596
Tigist Woldearegai <tigistlina@gmail.com>
2023-06-23 22:38:34 -0500

    creates board and card models, migrate db

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
+    # card = db.relationship("Card", back_populates="boards")
+    # card_id = db.Column(db.Integer, db.ForeignKey("card.id"))
