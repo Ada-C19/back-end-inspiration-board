29f957312fa6f8243994d77927420acabd19c1a1
Raissa Barbosa <raissaglaucie@icloud.com>
2023-06-24 14:49:06 -0400

    helper function to turn boards dictionary

--- app/models/board.py
+++ app/models/board.py
@@ -5,3 +5,14 @@ class Board(db.Model):
     title = db.Column(db.String)
     owner = db.Column(db.String)
     cards = db.relationship("Card", back_populates = "board", lazy = True)
+
+
+
+    def to_dict(self):
+        boards_dict = {
+        "id": self.board_id,
+        "title": self.title,
+        "owner": self.owner,
+
+        }
+        return boards_dict 
