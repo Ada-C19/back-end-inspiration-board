502c4777d5da3648e1dac8c68a04f69cc8920357
Rainacam <rainacam12@gmail.com>
2023-06-26 14:32:35 -0400

    connected card and board w/ foreignkey

--- app/models/card.py
+++ app/models/card.py
@@ -4,4 +4,5 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     liked_count = db.Column(db.Integer)
-   
+    board = db.relationship("Board", back_populates="cards")
+    board_fk = db.Column(db.Integer, db.ForeignKey('board.board_id'))
