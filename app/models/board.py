fad382344139a41b3117ec992fb2d35e16bf0126
Gweneth Johnson <johnsong0119@gmail.com>
2023-06-26 14:20:41 -0700

    added one to many relationship with board and card

--- app/models/board.py
+++ app/models/board.py
@@ -4,7 +4,7 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(255), nullable=False)
     owner= db.Column(db.String(255), nullable=False)
-
+    cards = db.relationship("Card", back_populates="board")
 def to_dict(self):
     return ({"board_id": self.board_id,
             "title": self.title,
--- app/models/card.py
+++ app/models/card.py
@@ -5,6 +5,7 @@ class Card(db.Model):
     message = db.Column(db.String(225), nullable=False)
     likes_count= db.Column(db.Integer, nullable=False )
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+    board = db.relationship("Board", back_populates="cards")
 
 def to_dict(self):
     return ({"card_id": self.card_id,
