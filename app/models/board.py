88ad783126cdb2c0bc0d504eb4201fd4b4e7534d
Gabby <gabrielleyoung10@gmail.com>
2023-06-26 17:59:52 -0500

    Add relationship between Board and Card models

--- app/models/board.py
+++ app/models/board.py
@@ -1,8 +1,8 @@
 from app import db
 
+
 class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-
-
+    cards = db.relationship("Card", back_populates="board")
--- app/models/card.py
+++ app/models/card.py
@@ -5,3 +5,5 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey("board.id"))
+    board = db.relationship("Board", back_populates="cards")
