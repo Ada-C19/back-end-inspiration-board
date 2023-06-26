2ce2c1b761c08b68efbbe4f07d4d23ba5b338744
an2084 <angelanawang47@gmail.com>
2023-06-26 13:34:19 -0400

    Created class Card model

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,6 @@
 from app import db
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincreme=True)
+    message = db.Column(db.String)
+    liked_count = db.Column(db.Integer)
