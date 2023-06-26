890c33a394784d180951b59c33ddd5727b3f7f7c
areeg <127351347+arejib@users.noreply.github.com>
2023-06-26 15:10:06 -0500

    Created and registered Blueprints for boards and cards

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,14 @@ def create_app():
 
     # Register Blueprints here
     # from .routes import example_bp
+    from .cards_routes import cards_bp
+    from .boards_routes import boards_bp
+
     # app.register_blueprint(example_bp)
+    app.register_blueprint(cards_bp)
+    app.register_blueprint(boards_bp)
+
+
 
     CORS(app)
     return app
