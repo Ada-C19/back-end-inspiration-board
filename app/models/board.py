2af393ef80e47c49cf3f65423c32d4f6cb9203ae
Lindsay <lindsaybolzwilson@gmail.com>
2023-06-26 11:54:54 -0400

    created basics for models and commented out the one-many relationship for dev reasons

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,10 @@
 from app import db
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    owner = db.Column(db.String)
+    title = db.Column(db.String)
+    # cards = db.relationship("Card", back_populates='board')
+
+
+
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,7 @@
 from app import db
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message = db.Column(db.String)
+    # board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
+    # board = db.relationship("Board", back_populates='cards')
