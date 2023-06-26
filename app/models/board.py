8d8059d9aabd06730d5f7995f3cc3d0a41056e0f
An Ba <ngwin.ab@gmail.com>
2023-06-26 12:21:48 -0400

    added models

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,5 @@
 from app import db
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    owner = db.Column(db.String, nullable=False)
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,5 @@
 from app import db
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message = db.Column(db.String, nullable = False)
+    likes_count = db.Column(db.Integer, nullable=True)
