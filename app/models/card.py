13c14d3d898f438a3f6cd97e5ee7d5d0b8dac5c4
Arianna Walden <nedlawa@gmail.com>
2023-06-26 16:36:09 -0700

    initial db setup with models

--- app/models/card.py
+++ app/models/card.py
@@ -1,15 +1,15 @@
 from app import db
 
 class Card(db.Model):
-    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
-    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=True)
     board = db.relationship("Board", back_populates="cards")
 
     def to_dict(self):
         card_dict = dict(
-            id=self.card_id,
+            id=self.id,
             message=self.message,
             likes_count=self.likes_count
         )
