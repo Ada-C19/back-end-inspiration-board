8d0a1be74bc7b5137137d0e9c0ff51f594091ef8
Raissa Barbosa <raissaglaucie@icloud.com>
2023-06-26 14:08:03 -0400

    create card routes get all cards and get one card by card_id

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -8,43 +8,43 @@ from .routes_helper import validate, validate_card
 cards_bp = Blueprint("card_bp", __name__, url_prefix="/cards")
 
 
-# @cards_bp.route("", methods=["POST"])
-# def create_card():
-#     request_body = request.get_json()
-#     if "board_id" not in request_body or "message" not in request_body:
-#         return make_response({"details": "data not in request body"}, 400)
+@cards_bp.route("", methods=["POST"])
+def create_card():
+    request_body = request.get_json()
+    if "board_id" not in request_body or "message" not in request_body:
+        return make_response({"details": "data not in request body"}, 400)
 
-#     try:
-#         board_id = request_body["board_id"]
-#         validate(Board, board_id)
-#         new_card = Card.from_dict(request_body)
-#         validate_card(new_card)
-#     except:
-#         return jsonify({"details": "Invalid Data"}), 400
+    try:
+        board_id = request_body["board_id"]
+        validate(Board, board_id)
+        new_card = Card.from_dict(request_body)
+        validate_card(new_card)
+    except:
+        return jsonify({"details": "Invalid Data"}), 400
 
-#     db.session.add(new_card)
-#     db.session.commit()
-#     return {
-#         "card": {
-#             "id": new_card.card_id,
-#             "message": new_card.message,
-#             "board_id": new_card.board_id
-#         }
-#     }, 201
-
-
-
-# @cards_bp.route("", methods=["GET"])
-# def get_all_cards():
-
-# #     cards = Card.query.all()
-# #     cards_response = [card.to_dict() for card in cards]
-
-# #     return jsonify(cards_response), 200
+    db.session.add(new_card)
+    db.session.commit()
+    return {
+        "card": {
+            "id": new_card.card_id,
+            "message": new_card.message,
+            "board_id": new_card.board_id
+        }
+    }, 201
 
 
-# # @ cards_bp.route("/<obj_id>", methods=["GET"])
-# # def get_one_card(obj_id):
-# #     card = validate(Card, obj_id)
 
-# #     return jsonify({"card": card.to_dict()})
+@cards_bp.route("", methods=["GET"])
+def get_all_cards():
+
+    cards = Card.query.all()
+    cards_response = [card.to_dict() for card in cards]
+
+    return jsonify(cards_response), 200
+
+
+@ cards_bp.route("/<obj_id>", methods=["GET"])
+def get_one_card(obj_id):
+    card = validate(Card, obj_id)
+
+    return jsonify({"card": card.to_dict()})
