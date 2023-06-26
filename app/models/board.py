f475c4e94dd7ada35e450d4fd9cf4eec29afc6c0
Rainacam <rainacam12@gmail.com>
2023-06-26 13:35:54 -0400

    board model added

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,7 @@
 from app import db
+from app import db
+
+class Board(db.board):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
