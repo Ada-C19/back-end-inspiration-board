a843fac82ee8f0af930b39a9a6ed103497ebc845
Gweneth Johnson <johnsong0119@gmail.com>
2023-06-27 10:58:47 -0700

    merge conflict stuff yeee

--- app/__init__.py
+++ app/__init__.py
@@ -19,20 +19,19 @@ def create_app():
 
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
-    from app.models.card import Card
     from app.models.board import Board
+    from app.models.card import Card
+    
+
+
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    from .routes.board_routes import board_bp
-    app.register_blueprint(board_bp)
-    from .routes.card_routes import cards_bp
-    app.register_blueprint(cards_bp)
-    
     # from .routes import example_bp
-    # app.register_blueprint(exampgile_bp)
+    # app.register_blueprint(example_bp)
+    
 
     CORS(app)
     return app
