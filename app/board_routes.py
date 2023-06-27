c188d54172ab16de7a3dedadd5a5c555389a130b
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-06-27 14:49:28 -0700

    implemented functionality to create a board

--- app/board_routes.py
+++ app/board_routes.py
@@ -4,3 +4,15 @@ from app.models.board import Board
 from app.models.card import Card
 
 bp = Blueprint('boards', __name__, url_prefix="/boards")
+
+# create a goal endpoint
+@bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+
+    new_goal = Board.from_dict(request_body)
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return jsonify({"goal": new_goal.to_dict()}), 201
