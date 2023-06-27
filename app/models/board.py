634ebecd4099dd8f79de508bec04c03c2fa99648
Jay King <jayriverking@gmail.com>
2023-06-27 10:45:56 -0400

    create DB relationship between models Board and Card

--- app/models/board.py
+++ app/models/board.py
@@ -4,3 +4,5 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
+    card = db.relationship("Card", back_populates="board")
+    card_id = db.Column(db.Integer, db.ForeignKey("card.card_id", ), nullable=True)
