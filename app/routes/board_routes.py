781603eb4e3985c1ec6f7c3b092010f609003638
Yvett Jimenez <yvett.nez@gmail.com>
2023-06-27 17:38:35 -0500

    jk on the last commit, THIS ONE has the functions template

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -8,3 +8,18 @@ board_bp = Blueprint("boards", __name__, url_prefix="/boards")
 @board_bp.route("", methods=["POST"])
 def create_board():
     pass
+
+# displays all boards
+@board_bp.route("", methods=["GET"])
+def get_all_boards():
+    pass
+
+# displays all cards for one board
+@board_bp.route("/<board_id>", methods=["GET"])
+def get_one_board():
+    pass
+
+# deletes a board (and associated cards)
+@board_bp.route("/<board_id>", methods=["DELETE"])
+def delete_one_board():
+    pass
