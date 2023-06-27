7e74ba391bf06df023720eeb1b3e753d96a300eb
Rainacam <rainacam12@gmail.com>
2023-06-27 10:28:01 -0400

    to_dict and from_dict funcs restored in board.py

--- app/models/board.py
+++ app/models/board.py
@@ -6,3 +6,21 @@ class Board(db.Model):
     title = db.Column(db.String)
     owner = db.Column(db.String)
     cards = db.relationship("Card", back_populates="board")
+
+    @classmethod
+        # data to give that will be organized into a dict 
+    def from_dict(cls, board_data):
+        new_board = Board(
+            title=board_data["title"],
+            owner=board_data["owner"]
+        )
+
+        return new_board
+
+    # turn response into dict, using this on an object
+    def to_dict(self):
+        return {
+            "board_id": self.board_id,
+            "title": self.title,
+            "owner": self.owner
+        }
