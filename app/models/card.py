26dffc0c88f5f3d3d6fcd297aaeab6bcbb315c6f
an2084 <angelanawang47@gmail.com>
2023-06-27 09:05:16 -0400

    Create from_dict and to_dict for CARD

--- app/models/card.py
+++ app/models/card.py
@@ -6,3 +6,21 @@ class Card(db.Model):
     liked_count = db.Column(db.Integer)
     board = db.relationship("Board", back_populates="cards")
     board_fk = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+
+
+@classmethod
+def from_dict(cls, card_data):
+    new_card = Card(
+        message = card_data["message"],
+        liked_count = card_data["liked_count"]
+    )
+
+    return new_card
+
+
+def to_dict(self):
+    return {
+        "card_id": self.card_id,
+        "message": self.message,
+        "liked_count": self.liked_count
+    }
