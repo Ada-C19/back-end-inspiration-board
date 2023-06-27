f01b7a736c4becfe52985e3ca7d3fe3daee587a1
Yvett Jimenez <yvett.nez@gmail.com>
2023-06-27 17:37:48 -0500

    create routes template for boardroutes file

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -3,3 +3,8 @@ from app import db
 
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+# creates a new board
+@board_bp.route("", methods=["POST"])
+def create_board():
+    pass
