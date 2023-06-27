3589da63e2cb55a86268d98253005f75ea7380cb
Zhuo <zhuo.parse@gmail.com>
2023-06-27 18:46:41 -0400

    created post route

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
+from app.models.board import Board
+
 
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint("boards", __name__, url_prefix="/boards")
@@ -7,7 +9,16 @@ board_bp = Blueprint("boards", __name__, url_prefix="/boards")
 # creates a new board
 @board_bp.route("", methods=["POST"])
 def create_board():
-    pass
+    request_body = request.get_json()
+
+    try:
+        new_board = Board.from_dict(request_body)
+        db.session.add(new_board)
+        db.session.commit()
+        return make_response({"board": new_board.to_dict()}, 201)
+    except KeyError:
+        abort(make_response({"Invalid data"}, 400))
+    
 
 # displays all boards
 @board_bp.route("", methods=["GET"])
