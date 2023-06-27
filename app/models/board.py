982b8f959950c9d71174bc17e9ce914be669028e
liz <lissete.trejo@gmail.com>
2023-06-26 18:03:02 -0700

    add model methods skeleton

--- app/models/board.py
+++ app/models/board.py
@@ -4,3 +4,11 @@ class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+
+    def to_dict(self):
+        pass
+
+    @classmethod
+    def from_dict(cls, data):
+        return Board(
+        )
--- app/models/card.py
+++ app/models/card.py
@@ -4,3 +4,14 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+
+    def to_dict(self):
+        card_dict = {
+
+        }
+        return card_dict
+    
+    @classmethod
+    def from_dict(cls, data):
+        return Card(
+        )
