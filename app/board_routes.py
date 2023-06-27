c1413621283b5df69e15837a17e99bc2ac881398
alyssa <tn.alyssa@gmail.com>
2023-06-26 21:42:34 -0500

    created file for route helpers and added functions for creating and validating model

--- app/board_routes.py
+++ app/board_routes.py
@@ -2,3 +2,41 @@ from flask import Blueprint, request, jsonify, make_response
 from app import db
 
 board_bp = Blueprint('board_bp', __name__, url_prefix="/boards")
+
+
+# CREATE
+@board_bp.route("", methods=["POST"])
+def post_board(): 
+    request_body = request.get_json()
+    new_board = create_model(Board, request_body)
+
+
+@board_bp.route("", methods=["POST"])
+def post_card_to_board():
+    pass
+
+# READ
+@board_bp.route("", methods=["GET"])
+def get_all_boards(): 
+    pass
+
+
+@board_bp.route("", methods=["GET"])
+def get_specific_board(): 
+    pass
+
+
+@board_bp.route("", methods=["GET"])
+def get_cards_from_board():
+    pass
+
+# UPDATE
+@board_bp.route("", methods=["PUT"])
+def update_board():
+    pass
+
+
+# DELETE
+@board_bp.route("", methods=["DELETE"])
+def delete_board():
+    pass
