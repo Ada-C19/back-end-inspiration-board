808d79a317ec75247d7509460715995e2e0da5c6
An Ba <ngwin.ab@gmail.com>
2023-06-27 14:54:30 -0400

    modified Card model

--- app/models/board.py
+++ app/models/board.py
@@ -3,6 +3,8 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     title = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
+    cards = db.relationship("Card",back_populates="board")
+
 
     @classmethod
     def from_dict_boards(cls, board_data):
--- app/models/card.py
+++ app/models/card.py
@@ -3,7 +3,8 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String, nullable = False)
     likes_count = db.Column(db.Integer, nullable=True)
-
+    board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"))
+    board = db.relationship("Board", back_populates = "cards")
 
     @classmethod
     def from_dict_cards(cls, card_data):
