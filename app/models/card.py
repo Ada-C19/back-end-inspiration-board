b04dc8e0a64ea6816287abc1e60ddc6f3aa4bafb
liz <lissete.trejo@gmail.com>
2023-06-26 18:15:51 -0700

    update migrations

--- app/models/card.py
+++ app/models/card.py
@@ -4,6 +4,9 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+    date_created = db.Column(db.DateTime, nullable=True)
+    board = db.relationship("Board", back_populates="cards")
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
 
     def to_dict(self):
         card_dict = {
