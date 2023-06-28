0cae6e24173aaf526fa5edd32aeb5e45aaad07f0
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-06-28 13:19:09 -0400

    add test config in __init__ file

---
+++ tests/test_routes.py
@@ -0,0 +1,12 @@
+import pytest
+from app.models.board import Board
+from app.models.card import Card
+
+def test_get_cards_no_saved_cards(client):
+    #Act
+    response = client.get("/cards")
+    response_body = response.get_json()
+
+    #Assert
+    assert response.status_code == 200
+    assert response_body == []
