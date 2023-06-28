e6195189e377d00035fe7b713151ef34a7e2b2e1
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-06-28 15:35:06 -0400

    add test for deleting a card

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,16 @@ def test_get_cards_no_saved_cards(client, one_board):
     #Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_delete_card(client, one_card):
+    #Act
+    response = client.delete("/cards/1")
+    response_body = response.get_json()
+
+    #Assert
+    assert response.status_code == 200
+    assert "details" in response_body
+    assert response_body == {
+        "details": "Card 1 successfully deleted"
+    }
+    assert Card.query.get(1) == None
