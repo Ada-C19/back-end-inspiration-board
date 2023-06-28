8258a37086d61a4ef321f846594e7863192f9f5c
Arianna Walden <nedlawa@gmail.com>
2023-06-28 11:20:52 -0700

    "update board_id in card model nullable to False"

--- app/models/card.py
+++ app/models/card.py
@@ -5,7 +5,7 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
-    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=True)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False)
     board = db.relationship("Board", back_populates="cards")
 
     def to_dict(self):
