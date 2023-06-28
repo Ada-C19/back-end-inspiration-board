c405849170d7c17ed85a072e54bc810b6ad1f7c8
Jay King <jayriverking@gmail.com>
2023-06-28 10:44:55 -0400

    fixed typo

--- app/models/board.py
+++ app/models/board.py
@@ -10,7 +10,7 @@ class Board(db.Model):
 
     def to_dict(self):
         board_as_dict = {}
-        board_as_dict_as_dict["id"] = self.id
+        board_as_dict_as_dict["id"] = self.board_id
         board_as_dict["title"] = self.title
         board_as_dict["owner"] = self.owner
         board_as_dict["cards"] = self.cards
