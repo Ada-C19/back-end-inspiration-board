4dac74987ee73cc8360f00ae37c008f64ba7548b
Zhuo <zhuo.parse@gmail.com>
2023-06-28 16:59:43 -0400

    created card post route

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -1,5 +1,33 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
+from app.models.card import Card
+from app.models.board import Board
+from app.routes.helper_functions import validate_model
 
 # example_bp = Blueprint('example_bp', __name__)
-card_bp = Blueprint("cards", __name__, url_prefix="/cards")
+card_bp = Blueprint("cards", __name__, url_prefix="/boards")
+
+@card_bp.route("/<board_id>", methods=["POST"])
+def create_one_card(board_id):
+    validate_model(Board, board_id)
+    request_body = request.get_json()
+    try:
+        new_card = Card.from_dict(request_body)
+        db.session.add(new_card)
+        db.session.commit()
+        return make_response({"card": new_card.to_dict()}, 201)
+    except KeyError:
+        abort(make_response({"details":"Invalid data"}, 400))
+
+
+
+@card_bp.route("<card_id>", methods=["PATCH"])
+def update_one_card_likes():
+
+
+@card_bp.route("<card_id>", methods=["DELETE"])
+def delete_one_card():
+
+
+
+
