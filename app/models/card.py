750bad6bfa6de1681ff3b09b55e1ae80b92dc28e
Jay King <jayriverking@gmail.com>
2023-06-28 10:39:40 -0400

    add self to dict function to board and card models

--- app/models/card.py
+++ app/models/card.py
@@ -9,8 +9,15 @@ class Card(db.Model):
     board_id = db.Column(db.Integer, db.ForeignKey(
         "board.board_id", ), nullable=True)
 
-def to_dict(self)
+# def to_dict(self)
+#     def to_dict(self):
+#             "id": self.id,
+#             "message": self.self,
+        
     def to_dict(self):
-            "id": self.id,
-            "message": self.self,
-          
+        card_as_dict = {}
+        card_as_dict["card_id"] = self.card_id
+        card_as_dict["message"] = self.message
+        card_as_dict["likes_count"] = self.likes_count
+        card_as_dict["board"] = self.board
+        return book_as_dict
