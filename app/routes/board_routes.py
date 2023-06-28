892b266af7ad8870b9cb0fd284417f44dc9966c9
liz <lissete.trejo@gmail.com>
2023-06-28 10:28:31 -0700

    revert routes so we can work on as a group

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -7,84 +7,32 @@ from flask import Blueprint, request, jsonify, make_response, abort
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 
 def validate_model(cls, model_id):
-    try:
-        model_id = int(model_id)
-    except:
-        abort(make_response(
-            jsonify({"message": f"{cls.__name__} {model_id} invalid"}), 400))
-        
-    model = cls.query.get(model_id)
-    if not model:
-        abort(make_response(
-            jsonify({"message": f"{cls.__name__} {model_id} not found"}), 400))
-    
-    return model 
-
+    pass
 
 @boards_bp.route("", methods=["POST"])
 def create_board():
-    request_body = request.get_json()
-
-    try: 
-        request_body["owner"] and request_body["title"] and request_body["description"]
-    except: 
-        abort(make_response(jsonify({"details": "Invalid data"}), 400))
-    
-    new_board = Board.from_dict(request_body)
-    
-    db.session.add(new_board)
-    db.session.commit()
-
-    response_body = {"board": new_board.to_dict()}
-
-    return make_response(jsonify(response_body), 201)
+    pass
 
 @boards_bp.route("", methods=["GET"])
 def get_all_boards():
-    boards = Board.query.all()
-    results = [board.to_dict() for board in boards]
-    return jsonify(results)
+    pass
 
 @boards_bp.route("/<board_id>", methods=["GET"])
 def get_board(board_id):
-    board = validate_model(Board, board_id)
-    response_body = {"board": board.to_dict()}
-    return response_body
+    pass
 
 @boards_bp.route("/<board_id>", methods=["DELETE"])
 def delete_board(board_id):
-    board_to_delete = validate_model(Board, board_id)
-    db.session.delete(board_to_delete)
-    db.session.commit()
-    response_body = {"details": f'Board {board_to_delete.id} "{board_to_delete.title}" succesfully deleted'}
-    return response_body
+    pass
 
 @boards_bp.route("/<board_id>", methods=["PATCH"])
 def update_board(board_id):
-    board_to_update = validate_model(Board, board_id)
-    board_updates = request.json()
-    board_to_update.message = board_updates["message"]
-    board_to_update.owner = board_updates["owner"]
-    board_to_update.description = board_updates["description"]
-    response_body = {board_to_update.to_dict()}
-    db.session.commit()
-    return response_body
-
+    pass
 
 @boards_bp.route("/<board_id>/cards", methods=["POST"])
 def create_card(board_id):
-    request_body = request.get_json()
-    new_card = Card.from_dict(request_body)
-    
-    db.session.add(new_card)
-    db.session.commit()
-
-    response_body = {"board": new_card.to_dict()}
-
-    return make_response(jsonify(response_body), 201)
+    pass
 
 @boards_bp.route("/<board_id>/cards", methods=["GET"])
 def get_all_cards(board_id):
-    board = validate_model(Board, board_id)
-    response_body = board.to_dict(cards = True)
-    return make_response(jsonify(response_body))
+    pass
--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -7,50 +7,23 @@ from flask import Blueprint, request, jsonify, make_response, abort
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 
 def validate_model(cls, model_id):
-    try:
-        model_id = int(model_id)
-    except:
-        abort(make_response(
-            jsonify({"message": f"{cls.__name__} {model_id} invalid"}), 400))
-        
-    model = cls.query.get(model_id)
-    if not model:
-        abort(make_response(
-            jsonify({"message": f"{cls.__name__} {model_id} not found"}), 400))
-    
-    return model 
-
+    pass
 
 @cards_bp.route("", methods=["GET"])
 def get_all_cards():
-    cards = Card.query.all()
-    results = [card.to_dict() for card in cards]
-    return jsonify(results)
+    pass
 
 @cards_bp.route("/<card_id>", methods=["GET"])
 def get_card(card_id):
-    card = validate_model(Card, card_id)
-    response_body = {"card": card.to_dict()}
-    return response_body
+    pass
 
 @cards_bp.route("/<card_id>", methods=["DELETE"])
 def delete_card(card_id):  
-    card_to_delete = validate_model(Card, card_id)
-    db.session.delete(card_to_delete)
-    db.session.commit()
-    response_body = {"details": f'Board {card_to_delete.id} "{card_to_delete.title}" succesfully deleted'}
-    return response_body
+    pass
 
 @cards_bp.route("/<card_id>", methods=["PATCH"])
 def update_card(card_id):
-    card_to_update = validate_model(Card, card_id)
-    card_updates = request.json()
-    card_to_update.title = card_updates["title"]
-    card_to_update.likes_count = card_updates["likes_count"]
-    card_to_update.date_created = card_updates["date_created"]
-    response_body = {card_to_update.to_dict()}
-    db.session.commit()
-    return response_body
+    pass
 
 # @cards_bp.route("/<card_id>/like", methods=["PATCH"])
 # def like_card(card_id):
