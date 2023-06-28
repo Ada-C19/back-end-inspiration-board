70a3f1516095749a7a749ab3a392eeadf10714cd
Muniba <mbashirkimes@gmail.com>
2023-06-28 13:08:25 -0400

    Added pytest fixtures to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 from app import create_app
 from app import db
+from app.models.board import Board
+from app.models.card import Card
+from datetime import datetime
+from flask.signals import request_finished
 
 
 @pytest.fixture
@@ -20,3 +24,28 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def one_card(app):
+    new_card = Card(
+        message="Reminder to water plants", likes_count=2)
+    db.session.add(new_card)
+    db.session.commit()
+
+@pytest.fixture
+def three_cards(app):
+    db.session.add_all([
+        Card(message="Reminder to water plants", likes_count=2),
+        Card(message="Reminder to do groceries", likes_count=1),
+        Card(message="Reminder to take out trash", likes_count=3)
+    ])
+    db.session.commit()
+
+@pytest.fixture
+def one_board_belongs_to_one_card(app, one_board, one_card):
+    card = Card.query.first()
+    board = Board.query.first()
+    board.cards.append(card)
+    db.session.commit()
+
+
