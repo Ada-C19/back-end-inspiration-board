6d39a693353bc7d887a07c89370626aacefafaa7
Jackie <jackiemay08@gmail.com>
2023-06-29 12:40:05 -0400

    added create one card test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,22 @@ def test_get_cards_no_saved_cards(client, one_board):
     #Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_create_one_card(client):
+    #Act
+    response = client.post("/boards/1/cards", json={
+        "message": "Test message",
+        "likes count": 0
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert "message" in response_body
+    assert response_body == {
+        "id": 1,
+        "message": "Test message",
+        "likes count": 0,
+        "board id": 1,
+        "board": "Board Title"
+    }
