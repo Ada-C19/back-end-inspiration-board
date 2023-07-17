b8ae80bc71d537f25cd6ac77898d2769f71fe59b
nadia <reach.nadia.saria@gmail.com>
2023-07-17 12:08:13 -0700

    fix CORS issue in init

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
-# from flask_cors import CORS
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -29,5 +29,7 @@ def create_app():
     from .routes.card_routes import bp
     app.register_blueprint(bp)
 
-    # CORS(app)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    
     return app
