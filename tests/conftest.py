0f6f73d4ba4bd0299998fd69c58528809a75d8d0
Genesis Quinn <genesis@quinns.io>
2023-07-18 14:11:02 -0400

    Card Tests G

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,22 @@
 import pytest
 from app import create_app
+from app.models.card import Card
+from app.models.board import Board
 from app import db
-
+from flask.signals import request_finished
 
 @pytest.fixture
 def app():
-    # create the app with a test config dictionary
     app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
 
     with app.app_context():
         db.create_all()
         yield app
 
-    # close and remove the temporary database
     with app.app_context():
         db.drop_all()
 
@@ -20,3 +24,38 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def one_board(app):
+    one_board = Board(
+        title="New Board", owner="Genesis", id=1)
+    db.session.add(one_board)
+    db.session.commit()
+    return one_board
+
+
+@pytest.fixture
+def one_card(app, one_board):
+    one_card = Card(
+        id=1,
+        message="Yoga",
+        likes_count=0,
+        board_id=1)
+    db.session.add(one_card)
+    db.session.commit()
+    return one_card
+
+
+
+# @pytest.fixture
+# def one_card_belongs_to_one_board(app, one_board, one_card):
+#     card = Card.query.first()
+#     board = Board.query.first()
+#     board.tasks.append(card)
+#     db.session.commit()
+#     return card, board 
+
+
+
+
