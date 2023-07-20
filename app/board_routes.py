278fc7b4af301bccd9ad21e04ce56359a60099f6
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-07-20 12:46:56 -0700

    fixed sorting by board id

--- app/board_routes.py
+++ app/board_routes.py
@@ -44,7 +44,8 @@ def retrieve_cards(board_id):
     board = validate_model(Board, board_id)
 
     sort_param = request.args.get("sort")
-    card_query = Card.query
+    card_query = Card.query.filter(Card.board_id==board.id)
+    print(card_query)
 
     if sort_param == "asc":
         card_query = card_query.order_by(Card.likes_count).all()
