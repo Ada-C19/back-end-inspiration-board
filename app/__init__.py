3877a383ab094f4fff44d87e0c458af76ac06a8b
alyssa <tn.alyssa@gmail.com>
2023-07-20 10:21:23 -0500

    remade migrations folder, configured app for render

--- app/__init__.py
+++ app/__init__.py
@@ -15,16 +15,17 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-        "SQLALCHEMY_DATABASE_URI")
+        "RENDER_DATABASE_URI")
+
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
     from app.models.board import Board
     from app.models.card import Card
 
-
-    db.init_app(app)
-    migrate.init_app(app, db)
 
     # Register Blueprints here
     # from .routes import example_bp
